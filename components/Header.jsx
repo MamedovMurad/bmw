dd2c7c05bd916d816c828d50336965f35f36d498
Murad Mammadov <murad.mammadov@hq.ferrumcapital.az>
2021-05-31 15:12:55 +0400

    search component removed

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import Style from '../styles/Header.module.css'
 import Navbar from './Navbar'
 import SearchComponent from './SearchComponent'
+import { useRouter } from 'next/router'
+
 function Header() {
+    const router = useRouter()
     return (
        <>
             <div className={Style.headerNavbar}>
                 <Navbar/>
-                <SearchComponent/>
+
+               {router.pathname!='/avtomobiller'&& <SearchComponent/>}
             </div>
        </>
     )
